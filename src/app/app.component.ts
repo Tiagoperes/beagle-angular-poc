6d260138a4b7297ebf3ea97e524e5c41239a6f5a
Tiago Peres França <tiagoperesfr@gmail.com>
2020-02-18 13:33:33 -0300

    updates beagle-angular lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'beagle-angular';
+  title = 'beagle-angular'
+  loadParams = {
+    path: 'd803e59aadc5c3cc8def28553f17d61f/raw/b7918f950ae6a0fb763f34f5637c76f8309fac7a/beagle-example.json',
+  }
 }
--- src/app/beagle.config.ts
+++ src/app/beagle.config.ts
@@ -12,10 +12,16 @@ import { LoadingComponent } from './components/loading/loading.component'
 import Schema from './schema'
 import { BeagleAngularConfig } from 'beagle-angular'
 
+function logger(uiTree) {
+  console.log(uiTree)
+  return uiTree
+}
+
 export const beagleConfig: BeagleAngularConfig<Schema> = {
   baseUrl: 'https://gist.githubusercontent.com/Tiagoperes',
   schemaUrl: 'https://gist.githubusercontent.com/Tiagoperes/df605a1656f27f7c2685a3c55979029b/raw/b438cd2831b0032de37fe9a1b2c320f30a3d3c4f/beagle-schema.ts',
   module: BeagleComponentsModule,
+  middlewares: [logger],
   components: {
     button: ButtonComponent,
     container: ContainerComponent,
